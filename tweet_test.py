dfbcd90577339abd0caf8dae35739e69b5f9314c
Jason Huang <jason_huang@linkwish.com>
2023-10-24 00:08:07 +0800

    append footer by each sns source

--- tweet_test.py
+++ tweet_test.py
@@ -1,11 +1,14 @@
+import asyncio
+
 ***REMOVED***
 ***REMOVED***_bot
 
-tweet_url = "https://twitter.com/CUBE_LIGHTSUM/status/1715235370747744475"
+tweet_url = "https://twitter.com/_EL7ZUPofficial/status/1716452439426314316"
 
 webhook_url = twitter_crawler.get_discord_webhook(tweet_url)
 # 測試
 # webhook_url = "https://discord.com/api/webhooks/1162632449545752597/sCvieQPZNw5G9XX1iPS-N3WusXnPfrcUU3YjPHlnBzI_CepxO7t4jGKFlRVFDvJNVhNc"
 tweet_data = twitter_crawler.fetch_data_from_tweet(tweet_url)
 
-discord_bot.send_message(webhook_url=webhook_url, sns_info=tweet_data)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(discord_bot.send_message(webhook_url=webhook_url, sns_info=tweet_data))
--- weverse_test.py
+++ weverse_test.py
@@ -1,9 +1,11 @@
+import asyncio
+
 ***REMOVED***_bot
 ***REMOVED***
 
-fetch_url = "https://weverse.io/stayc/artist/3-137376228"
+fetch_url = "https://weverse.io/wooah/artist/4-137746729"
 
-webhook_url = weverse_crawler.get_discord_webhook(fetch_url)
+webhook_url = weverse_crawler.get_discord_webhook("_EL7ZUPofficial")
 
 # 測試
 # webhook_url = "https://discord.com/api/webhooks/1162632449545752597/sCvieQPZNw5G9XX1iPS-N3WusXnPfrcUU3YjPHlnBzI_CepxO7t4jGKFlRVFDvJNVhNc"
@@ -13,4 +15,5 @@ data = weverse_crawler.fetch_data_from_weverse(fetch_url)
 # https://weverse.io/stayc/artist/3-137359149
 # https://weverse.io/stayc/artist/3-137376228
 
-discord_bot.send_message(webhook_url=webhook_url, sns_info=data)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(discord_bot.send_message(webhook_url=webhook_url, sns_info=data))
