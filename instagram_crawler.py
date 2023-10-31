f8d6682ff79e5f1515356a5b942fea860a14df56
jason_huang <jason_huang@staff.1111.com.tw>
2023-10-31 15:53:21 +0800

    fix Instagram crawler error

--- instagram_crawler.py
+++ instagram_crawler.py
@@ -1,36 +1,31 @@
 from instagrapi import Client
 
 from SnsInfo import SnsInfo, Profile
-from discord_bot ***REMOVED***_webhook
 
 
-def fetch_data_from_instagram(url: str):
-    cl = Client()
-
-    # user_id = cl.user_id_from_username(ACCOUNT_USERNAME)
-    # medias = cl.user_medias(user_id, 20)
-
-    # https://www.instagram.com/p/CynPhAMJpz0/?igshid=MzRlODBiNWFlZA==
-
+def fetch_data_from_instagram(cl: Client, url: str):
     media_pk = cl.media_pk_from_url(url)
     post_info = cl.media_info(media_pk)
 
-    # 文章內容 caption_text
-    # user.username profile_pic_url
-    # resources.thumbnail_url video_url
-    # like_count
+    user_full_name = post_info.user.full_name
+    username = post_info.user.username
+    profile_pic_url = post_info.user.profile_pic_url
+    caption_text = post_info.caption_text
+    profile = Profile(name=f"{user_full_name} (@{username})", url=profile_pic_url)
 
-    image_links = [resource.thumbnail_url for resource in post_info.resources]
-
-    return SnsInfo(post_link=url, profile=Profile(name=f"{post_info.user.full_name} (@{post_info.user.username})",
-                                                  url=post_info.user.profile_pic_url), content=post_info.caption_text,
-                   images=image_links)
-
-
-def get_discord_webhook(id: str):
-    if id == "47318740444":  # LIGHTSUM
-***REMOVED*** discord_webhook("LIGHTSUM")
-    elif id == "42987402481":  # STAYC
-***REMOVED*** discord_webhook("STAYC")
-    elif id == "61470632061":  # EL7Z UP
-***REMOVED*** discord_webhook("EL7Z UP")
+    media_type = post_info.media_type
+    if media_type == 1:  # single photo https://www.instagram.com/p/CwDaXh-vnUY
+***REMOVED*** SnsInfo(post_link=url, profile=profile, content=caption_text, images=[post_info.thumbnail_url])
+    elif media_type == 2:  # single video https://www.instagram.com/reel/CzA_bJpyL7M
+***REMOVED*** SnsInfo(post_link=url, profile=profile, content=caption_text, images=[post_info.thumbnail_url],
+                       videos=[post_info.video_url])
+    elif media_type == 8:  # album (include photos or videos) https://www.instagram.com/p/Cxnbj7sS1Ns
+        images = []
+        videos = []
+        for resource in post_info.resources:
+            if resource.media_type == 1:
+                images.append(resource.thumbnail_url)
+            elif resource.media_type == 2:
+                images.append(resource.thumbnail_url)
+                videos.append(resource.video_url)
+***REMOVED*** SnsInfo(post_link=url, profile=profile, content=caption_text, images=images, videos=videos)
