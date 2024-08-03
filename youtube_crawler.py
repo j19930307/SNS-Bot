e0ee388adddc4661c49150da056bbaeee2547a45
Jason Huang <jason_huang@linkwish.com>
2024-08-03 09:33:58 +0800

    fix fetch streams id error

--- youtube_crawler.py
+++ youtube_crawler.py
@@ -84,7 +84,9 @@ def get_latest_streams(channel_handle: str):
             ytVariableData = json.loads(scriptContent.split(ytVariableDeclaration)[1][:-1])
             break
 
-    tabRenderer = ytVariableData['contents']['twoColumnBrowseResultsRenderer']['tabs'][3]['tabRenderer']
+    tabRenderer = ytVariableData['contents']['twoColumnBrowseResultsRenderer']['tabs'][3].get('tabRenderer', None)
+    if not tabRenderer:
+        return []
     url = tabRenderer['endpoint']['commandMetadata']['webCommandMetadata']['url']
     match = re.search(r'/([^/]+)$', url)
 
